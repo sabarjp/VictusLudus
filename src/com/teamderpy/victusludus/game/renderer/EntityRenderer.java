c7bba03322f82796588deba80e0ca3769c064232
jpowers_pc <sabareq@yahoo.com>
2013-04-29 19:14:20 -0500

    added some missing renderer files

--- src/com/teamderpy/victusludus/game/renderer/EntityRenderer.java
+++ src/com/teamderpy/victusludus/game/renderer/EntityRenderer.java
@@ -55,7 +55,7 @@ public class EntityRenderer {
 
 		//check if a ray of light can hit the base of this entity
 		int[] sc = RenderUtil.worldCoordToRawUnscaledScreenCoord(gameEntity.getPos().getX(), gameEntity.getPos().getY(), gameEntity.getPos().getZ());
-		System.err.println("base of entity is " + gameEntity.getPos() + "  " + sc[0] + "," + sc[1]);
+		//System.err.println("base of entity is " + gameEntity.getPos() + "  " + sc[0] + "," + sc[1]);
 
 		GameTile[][][] tileArray = this.gameRenderer.game.getMap().getMap();
 
@@ -66,16 +66,16 @@ public class EntityRenderer {
 		int[] yOffsets = new int[gameEntity.getEntity().getHeight()];
 		for(int i=0; i<gameEntity.getEntity().getHeight(); i++){
 			yOffsets[i] = sc[1] + (i+1) * this.gameRenderer.game.getGameDimensions().getLayerHeight();
-			System.err.println("  offset " + i + "  at " + yOffsets[i]);
+			//System.err.println("  offset " + i + "  at " + yOffsets[i]);
 		}
 
 		boolean isBlocked = false;
 		//start from the top of the entity and go down
 		for(int i=yOffsets.length-1; i>=0; i--){
-			System.err.println("  checking against offset " + i + " at " + yOffsets[i]);
+			//System.err.println("  checking against offset " + i + " at " + yOffsets[i]);
 			isBlocked = false;
 
-			System.err.println("  making sure that offset " + (i+1+z) + " isnt over current layer " + layer);
+			//System.err.println("  making sure that offset " + (i+1+z) + " isnt over current layer " + layer);
 			if(i+1 + z > layer){
 				//this entity must be visible since nothing could break sight of its top
 				break;
@@ -84,7 +84,7 @@ public class EntityRenderer {
 			for(int k=layer; k>z+i+1; k--){
 				//a certain offset can only be blocked by certain tiles
 				int[] wc = RenderUtil.screenCoordToRawUnscaledWorldCoord(sc[0], yOffsets[i], k);
-				System.err.println("    for the layer " + k + " the potential tile is " + wc[0] + "," + wc[1] + "," + wc[2]);
+				//System.err.println("    for the layer " + k + " the potential tile is " + wc[0] + "," + wc[1] + "," + wc[2]);
 
 				GameTile t = null;
 
@@ -92,18 +92,18 @@ public class EntityRenderer {
 					t = tileArray[k][wc[0]][wc[1]];
 
 					if(t != null){
-						System.err.println("       a blocking tile exists");
+						//System.err.println("       a blocking tile exists");
 						//this is blocked, so go to the next spot
 						isBlocked = true;
 						break;
 					}
 
-					System.err.println("       no blocking tile exists");
+					//System.err.println("       no blocking tile exists");
 				}
 
 				//if the top of the entity if visible then add it
 				if(!isBlocked){
-					System.err.println("       the offset " + i + " is visible");
+					//System.err.println("       the offset " + i + " is visible");
 					if(!this.culledEntityList.contains(gameEntity)){
 						this.culledEntityList.add(gameEntity);
 					}
@@ -114,12 +114,12 @@ public class EntityRenderer {
 
 		//if we got all the way through then the entity is fully blocked off, so remove it
 		if(isBlocked){
-			System.err.println("  blocked all the way through");
+			//System.err.println("  blocked all the way through");
 			if(this.culledEntityList.contains(gameEntity)){
 				this.culledEntityList.remove(gameEntity);
 			}
 		} else {
-			System.err.println("  visible at the last second");
+			//System.err.println("  visible at the last second");
 			if(!this.culledEntityList.contains(gameEntity)){
 				this.culledEntityList.add(gameEntity);
 			}
